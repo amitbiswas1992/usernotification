8ab74049b17c8bd2b0fd8be633c6bdac4a7e614e
Amit Biswas <biswas1992@hotmail.com>
2019-07-17 21:49:42 -0400

    Initial Commit

---
+++ NotificationsAppTests/NotificationsAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  NotificationsAppTests.swift
+//  NotificationsAppTests
+//
+//  Created by Maze Geek on 7/17/19.
+//  Copyright © 2019 Amit Biswas. All rights reserved.
+//
+
+import XCTest
+@testable import NotificationsApp
+
+class NotificationsAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
